4f7682d02b6b92dee7b870cf307863fee4c30a7d
Sky <iflyinskyin2013@gmail.com>
2018-07-27 11:05:34 +0800

    fix  ylog's  and ycurl 's bugs

--- examples/test_ylog.cpp
+++ examples/test_ylog.cpp
@@ -4,10 +4,26 @@
 
 int main (int argc, char * argv[]){
 
+    yLib::yLog::I("class call");
+    yLib::yLog::D("class call A %d, %f test ylog", 1234, 66.5555);
+    yLib::yLog::W("class call A %d, %f test ylog", 1234, 66.5555);
+    yLib::yLog::E("class call A %d, %f test ylog", 1234, 66.5555);
+
     yLib::yLog test;
-    test.I("A %d, %f test ylog", 123, 54.5555);
-    test.D("A %d, %f test ylog", 123, 54.5555);
-    test.W("A %d, %f test ylog", 123, 54.5555);
-    test.E("A %d, %f test ylog", 123, 54.5555);
+    test.I("object call");
+    test.D("object call A %d, %f test ylog", 123, 54.5555);
+    test.W("object call A %d, %f test ylog", 123, 54.5555);
+
+
+    yLib::yLog::I("class call");
+    yLib::yLog::D("class call A %d, %f test ylog", 1234, 66.5555);
+    yLib::yLog::W("class call A %d, %f test ylog", 1234, 66.5555);
+    yLib::yLog::E("class call A %d, %f test ylog", 1234, 66.5555);
+
+
+    test.I("object call");
+    test.E("object call A %d, %f test ylog", 123, 54.5555);
+
+
     return 0;
 }
