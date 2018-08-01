86f69bc490d71b53ba37f997539ab1e7acd5c21e
Sky <iflyinskyin2013@gmail.com>
2018-08-01 10:23:09 +0800

    yxml-module complement

--- examples/test_yxml.cpp
+++ examples/test_yxml.cpp
@@ -16,6 +16,14 @@ int main (int argc, char * argv[]){
 
     xml.yXml_Set_Val(node_name, node_prop_name, node_prop_val, child_node_name, child_node_val);
 
+    node_name = "regist_device";
+    node_prop_name = "id";
+    node_prop_val = "hhh";
+    child_node_name = "serial_num";
+    xml.yXml_Get_Val(node_name, node_prop_name, node_prop_val, child_node_name, child_node_val);
+
+    yLib::yLog::I(("serial_num = "+child_node_val).c_str());
+
     std::string file1 = "test.xml";
     xml.yXml_Write(file1);
     return 0;
--- src/yxml.cpp
+++ src/yxml.cpp
@@ -44,8 +44,121 @@ int yLib::yXML::yXml_Read(std::string & xml_path){
 	// xmlSaveFormatFileEnc(_xmlfile_path.c_str(), pdoc, "UTF-8", 1);
     return 0;
 }
+static xmlNodePtr yXml_Tree_Traversal(xmlNodePtr ptr_node, \
+    std::string &node_name, \
+    std::string &node_prop_name, \
+    std::string &node_prop_val, \
+    std::string & child_node_name){
 
-int yLib::yXML::yXml_Set_Val(std::string &node_name, std::string &node_prop_name, std::string &node_prop_val, std::string & child_node_name, std::string child_node_val){
+	xmlNodePtr child_node;
+
+
+	while ( NULL != ptr_node ){
+
+		//yLib::yLog::I((char *)ptr_node->name);
+		if (!xmlStrcmp(ptr_node->name, BAD_CAST("comment"))){
+			//Attention:
+			//we deal with xml-comment-line
+			//if xml node_name is comment,we do nothing
+		}
+		if (!xmlStrcmp(ptr_node->name, BAD_CAST(node_name.c_str()))){
+			
+			xmlChar *name = xmlGetProp(ptr_node, BAD_CAST(node_prop_name.c_str()));
+			if ( !xmlStrcmp(name, BAD_CAST(node_prop_val.c_str())) ){
+
+				yLib::yLog::E(("find prop "+node_prop_val).c_str());
+
+				xmlNodePtr pcur_children = ptr_node->xmlChildrenNode;
+
+				while (pcur_children != NULL)
+				{
+					if (!xmlStrcmp(pcur_children->name, BAD_CAST(child_node_name.c_str())))
+					{
+						char str[1024]{0};
+						sprintf(str, "%s", ((char*)XML_GET_CONTENT(pcur_children->xmlChildrenNode)));
+						yLib::yLog::I(("read node val is " + std::string(str)).c_str());
+						//xmlNodeSetContent(pcur_children->children, BAD_CAST(child_node_val.c_str()));
+						//child_node_val = (char*)XML_GET_CONTENT(pcur_children->xmlChildrenNode);
+						//xmlNodeGetContent
+						return pcur_children;
+						break;//break while loop
+					}
+
+					pcur_children = pcur_children->next;
+				}
+
+				break; // break while loop
+			}
+
+		}
+		if ( NULL != ptr_node->xmlChildrenNode )
+			yXml_Tree_Traversal(ptr_node->xmlChildrenNode, node_name, \
+					node_prop_name, node_prop_val, \
+					child_node_name);
+		ptr_node = ptr_node->next;
+	}
+
+	return NULL;
+}
+
+static void yXml_Tree_Traversal(xmlNodePtr ptr_node, \
+    std::string &node_name, \
+    std::string &node_prop_name, \
+    std::string &node_prop_val, \
+    std::string & child_node_name, \
+    std::string &child_node_val){
+
+    xmlNodePtr child_node;
+
+
+	while ( NULL != ptr_node ){
+
+		//yLib::yLog::I((char *)ptr_node->name);
+		if (!xmlStrcmp(ptr_node->name, BAD_CAST("comment"))){
+			//Attention:
+			//we deal with xml-comment-line
+			//if xml node_name is comment,we do nothing
+		}
+		if (!xmlStrcmp(ptr_node->name, BAD_CAST(node_name.c_str()))){
+			
+			xmlChar *name = xmlGetProp(ptr_node, BAD_CAST(node_prop_name.c_str()));
+			if ( !xmlStrcmp(name, BAD_CAST(node_prop_val.c_str())) ){
+
+				yLib::yLog::E(("find prop "+node_prop_val).c_str());
+
+				xmlNodePtr pcur_children = ptr_node->xmlChildrenNode;
+
+				while (pcur_children != NULL)
+				{
+					if (!xmlStrcmp(pcur_children->name, BAD_CAST(child_node_name.c_str())))
+					{
+						char str[1024]{0};
+						sprintf(str, "%s", ((char*)XML_GET_CONTENT(pcur_children->xmlChildrenNode)));
+						yLib::yLog::I(("read node val is " + std::string(str)).c_str());
+						xmlNodeSetContent(pcur_children->children, BAD_CAST(child_node_val.c_str()));
+						//child_node_val = (char*)XML_GET_CONTENT(pcur_children->xmlChildrenNode);
+						//xmlNodeGetContent
+						break;//break while loop
+					}
+
+					pcur_children = pcur_children->next;
+				}
+
+				break; // break while loop
+			}
+
+		}
+		if ( NULL != ptr_node->xmlChildrenNode )
+			yXml_Tree_Traversal(ptr_node->xmlChildrenNode, node_name, \
+					node_prop_name, node_prop_val, \
+					child_node_name, child_node_val);
+		ptr_node = ptr_node->next;
+	}
+
+
+}
+
+int yLib::yXML::yXml_Set_Val(std::string &node_name, std::string &node_prop_name, std::string &node_prop_val, std::string & child_node_name, std::string & child_node_val) const {
 
 	xmlNodePtr proot = NULL, pcur = NULL;
 	/*****************获取xml文档对象的根节对象********************/
@@ -58,38 +171,40 @@ int yLib::yXML::yXml_Set_Val(std::string &node_name, std::string &node_prop_name
 	}
 
 	pcur = proot->xmlChildrenNode;
-	while (pcur != NULL)
+    yXml_Tree_Traversal(pcur, node_name, \
+        node_prop_name, node_prop_val, \
+        child_node_name, child_node_val);
+
+    return 0;
+}
+
+int yLib::yXML::yXml_Get_Val(std::string &node_name, std::string &node_prop_name, std::string &node_prop_val, std::string & child_node_name, std::string & child_node_val) const {
+
+	xmlNodePtr proot = NULL, pcur = NULL;
+	/*****************获取xml文档对象的根节对象********************/
+	proot = xmlDocGetRootElement(_xmlfile_pdoc_);
+
+	if (proot == NULL)
 	{
-		if (!xmlStrcmp(pcur->name, BAD_CAST(node_name.c_str())))
-		{
-
-			xmlChar *name = xmlGetProp(pcur, BAD_CAST(node_prop_name.c_str()));
-
-			if ( !xmlStrcmp(name, BAD_CAST(node_prop_val.c_str())) ){
-
-				yLib::yLog::E(("find prop"+node_prop_val).c_str());
-				xmlNodePtr pcur_children = pcur->xmlChildrenNode;
-
-				while (pcur_children != NULL)
-				{
-					if (!xmlStrcmp(pcur_children->name, BAD_CAST(child_node_name.c_str())))
-					{
-						char str[1024]{0};
-						sprintf(str, "%s", ((char*)XML_GET_CONTENT(pcur_children->xmlChildrenNode)));
-						yLib::yLog::I(("read node val is " + std::string(str)).c_str());
-						xmlNodeSetContent(pcur_children->children, BAD_CAST(child_node_val.c_str()));
-					}
-
-					pcur_children = pcur_children->next;
-				}
-			}
-
-			xmlFree(name);
-
-		}
-
-		pcur = pcur->next;
+		yLib::yLog::E("error: file is empty!\n");
+		return -1;
 	}
+
+	pcur = proot->xmlChildrenNode;
+	xmlNodePtr find_val;
+    find_val = yXml_Tree_Traversal(pcur, node_name, \
+        node_prop_name, node_prop_val, \
+        child_node_name);
+
+	if ( NULL != find_val ){
+
+		child_node_val = (char*)XML_GET_CONTENT(find_val->xmlChildrenNode);
+	}
+	else{
+
+		yLib::yLog::W("Can not find appropriate result");
+	}
+
     return 0;
 }
 
