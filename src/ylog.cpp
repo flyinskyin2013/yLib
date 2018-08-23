2ce95250bab64b97d72364633c2f16f57337cbc3
Sky <iflyinskyin2013@gmail.com>
2018-08-23 10:51:42 +0800

    yLog support process-safety

--- src/ylog.cpp
+++ src/ylog.cpp
@@ -4,7 +4,9 @@ char  yLib::yLog::m_ptr_msg_buf[] = {'\0'};
 //bool yLib::yLog::m_b_is_class_access = true;
 log4cpp::Category * yLib::yLog::root = nullptr;
 bool yLib::yLog::m_enable_log4cpp = false;
+bool yLib::yLog::m_enable_feature_ps = false;
 pthread_mutex_t yLib::yLog::m_mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t yLib::yLog::m_process_mutex;
 
 yLib::yLog::yLog(bool enable_log4cpp , std::string log_path){
 
@@ -32,6 +34,7 @@ yLib::yLog::~yLog(){
     if ( m_enable_log4cpp ){
         //log4cpp
         log4cpp::Category::shutdown();
+        m_enable_log4cpp = false;
     }
 }
 
@@ -40,7 +43,14 @@ void yLib::yLog::I(const char * fmt , ...){
     // if ( m_b_is_class_access )
     //     m_ptr_msg_buf = new char[MSG_BUF_SIZE];
 
-    pthread_mutex_lock(&m_mutex);
+    if ( m_enable_feature_ps ){
+
+        pthread_mutex_lock(&m_process_mutex);
+    }
+    else{ // thread safety
+
+        pthread_mutex_lock(&m_mutex);
+    }
 
     memset(m_ptr_msg_buf, 0, MSG_BUF_SIZE);
 
@@ -68,7 +78,14 @@ void yLib::yLog::I(const char * fmt , ...){
     //     m_ptr_msg_buf = nullptr;
     // }
 
-    pthread_mutex_unlock(&m_mutex);
+    if ( m_enable_feature_ps ){
+
+        pthread_mutex_unlock(&m_process_mutex);
+    }
+    else{//thread safety
+
+        pthread_mutex_unlock(&m_mutex);
+    }
 }
 
 void yLib::yLog::D(const char * fmt , ...){
@@ -76,7 +93,14 @@ void yLib::yLog::D(const char * fmt , ...){
     // if ( m_b_is_class_access )
     //     m_ptr_msg_buf = new char[MSG_BUF_SIZE];
 
-    pthread_mutex_lock(&m_mutex);
+    if ( m_enable_feature_ps ){
+
+        pthread_mutex_lock(&m_process_mutex);
+    }
+    else{ // thread safety
+
+        pthread_mutex_lock(&m_mutex);
+    }
 
     memset(m_ptr_msg_buf, 0, MSG_BUF_SIZE);
 
@@ -104,7 +128,14 @@ void yLib::yLog::D(const char * fmt , ...){
     //     m_ptr_msg_buf = nullptr;
     // }
 
-    pthread_mutex_unlock(&m_mutex);
+    if ( m_enable_feature_ps ){
+
+        pthread_mutex_unlock(&m_process_mutex);
+    }
+    else{//thread safety
+
+        pthread_mutex_unlock(&m_mutex);
+    }
 }
 
 void yLib::yLog::W(const char * fmt , ...){
@@ -112,7 +143,14 @@ void yLib::yLog::W(const char * fmt , ...){
     // if ( m_b_is_class_access )
     //     m_ptr_msg_buf = new char[MSG_BUF_SIZE];
 
-    pthread_mutex_lock(&m_mutex);
+    if ( m_enable_feature_ps ){
+
+        pthread_mutex_lock(&m_process_mutex);
+    }
+    else{ // thread safety
+
+        pthread_mutex_lock(&m_mutex);
+    }
 
     memset(m_ptr_msg_buf, 0, MSG_BUF_SIZE);
 
@@ -141,15 +179,29 @@ void yLib::yLog::W(const char * fmt , ...){
     //     m_ptr_msg_buf = nullptr;
     // }
 
-    pthread_mutex_unlock(&m_mutex);
+    if ( m_enable_feature_ps ){
+
+        pthread_mutex_unlock(&m_process_mutex);
+    }
+    else{//thread safety
+
+        pthread_mutex_unlock(&m_mutex);
+    }
 }
 
 void yLib::yLog::E(const char * fmt , ...){
 
     // if ( m_b_is_class_access )
     //     m_ptr_msg_buf = new char[MSG_BUF_SIZE];
+    if ( m_enable_feature_ps ){
 
-    pthread_mutex_lock(&m_mutex);
+        pthread_mutex_lock(&m_process_mutex);
+    }
+    else{ // thread safety
+
+        pthread_mutex_lock(&m_mutex);
+    }
+    
 
     memset(m_ptr_msg_buf, 0, MSG_BUF_SIZE);
 
@@ -177,9 +229,52 @@ void yLib::yLog::E(const char * fmt , ...){
     //     m_ptr_msg_buf = nullptr;
     // }
 
-    pthread_mutex_unlock(&m_mutex);
+    if ( m_enable_feature_ps ){
+
+        pthread_mutex_unlock(&m_process_mutex);
+    }
+    else{//thread safety
+
+        pthread_mutex_unlock(&m_mutex);
+    }
+    
 }
 
-void yLib::yLog::SetLog4cpp(bool enable_log4cpp){
-    m_enable_log4cpp = enable_log4cpp;
+// void yLib::yLog::SetLog4cpp(bool enable_log4cpp){
+
+//     m_enable_log4cpp = enable_log4cpp;
+// }
+
+void yLib::yLog::EnableFeatureProcessSafety(bool enable_feature){
+
+    pthread_mutexattr_t mtx_attr;
+
+    pthread_mutexattr_init(&mtx_attr);
+
+    int ret = 0;
+    if ( 0 < (ret = pthread_mutexattr_setpshared(&mtx_attr, PTHREAD_PROCESS_SHARED) ) ){
+        
+        if ( ENOTSUP == ret ){
+
+            E("pthread_mutexattr_setpshared, pshared is PTHREAD_PROCESS_SHARED but the implementation does not support process-shared mutexes.");
+        }
+        return;
+    }
+
+    if ( enable_feature ){
+
+        pthread_mutex_init(&m_process_mutex, & mtx_attr);
+        m_enable_feature_ps = true;
+    } 
+    else{
+        if ( m_enable_feature_ps ){
+
+            m_enable_feature_ps = false;
+            pthread_mutex_destroy(&m_process_mutex);
+        }
+        else
+            yLib::yLog::W("Feature of process-safety do not enable.");
+    }
+
+    pthread_mutexattr_destroy(&mtx_attr);
 }
