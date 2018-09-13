d91e2a3ad63873642bd0ebbd39596056ac04af65
Sky <iflyinskyin2013@gmail.com>
2018-09-13 10:26:53 +0800

    fix a bug of CreateCAndC++FileWithComment.sh

--- examples/ShellScriptExamples/CreateCAndC++FileWithComment.sh
+++ examples/ShellScriptExamples/CreateCAndC++FileWithComment.sh
@@ -10,4 +10,9 @@ echo "Create file "$1" ... ..."
 touch $1
 
 echo "Add comment for file "$1" ... ..."
-bash ./add-comment.sh $1
+
+#cd $(dirname $0)
+
+sh_path=$(cd $(dirname $0);pwd)
+bash ${sh_path}/add-comment.sh $1
+
