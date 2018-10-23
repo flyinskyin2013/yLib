c9e0a642e977ad376e4469ac16ef7c3a2e9c34dc
Sky <iflyinskyin2013@gmail.com>
2018-10-23 11:21:23 +0800

    optimize ylib and simplize header-files dependence of ylib

---
+++ include/ylib.hpp
@@ -0,0 +1,12 @@
+#ifndef _YLIB_H_
+#define _YLIB_H_
+
+#include <core/ycommon.hpp>
+#include <core/ycurl.hpp>
+#include <core/ylog.hpp>
+#include <core/yshell.hpp>
+#include <core/yxml.hpp>
+#include <core/yconfig.hpp>
+
+
+#endif //_YLIB_H_
