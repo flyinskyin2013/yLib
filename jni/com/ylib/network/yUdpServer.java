169ad120c00650769fbd8013b7a370c8ce9ca44a
Sky <iflyinskyin2013@gmail.com>
2020-11-18 16:50:23 +0800

    1 Add some experimental features. 2 Fix a bug at yLib::yLog::SetLog4cpp() when the cfg file is invalid.

---
+++ jni/com/ylib/network/yUdpServer.java
@@ -0,0 +1,16 @@
+/*
+ * @Author: Sky
+ * @Date: 2020-10-26 18:34:19
+ * @LastEditors: Sky
+ * @LastEditTime: 2020-10-26 18:34:51
+ * @Description: 
+ */
+
+package com.ylib.network;
+
+public class yUdpServer {
+    static {
+		
+		System.loadLibrary("libylib.so");
+    }
+}
