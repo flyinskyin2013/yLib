20408cf7799bbb524c2fbefc63910c5a2a1d6ed6
Sky <iflyinskyin2013@gmail.com>
2020-12-03 14:35:12 +0800

    1 hide some namespace of third-part. 2 add comment for yShell

--- include/utility/yshell.hpp
+++ include/utility/yshell.hpp
@@ -2,7 +2,7 @@
  * @Author: Sky
  * @Date: 2018-10-23 11:13:37
  * @LastEditors: Sky
- * @LastEditTime: 2020-07-20 15:14:26
+ * @LastEditTime: 2020-12-03 14:33:37
  * @Description: 
  */
 
@@ -33,14 +33,42 @@ extern "C"{
 
 namespace yLib{
 
-
+    /**
+     *  @class yShell
+     *  @brief This class can run our cmd in linux(it is based on popen/pclose).
+     *  @warning We will support windows in the future.
+     */
     class yShell MACRO_PUBLIC_INHERIT_YOBJECT{
 
         public:
+        /**
+         *  @fn    yShell()
+         *  @brief Default constructor
+         */
         yShell();
+
+        /**
+         *  @fn    ~yShell()
+         *  @brief Default destructor
+         */
         ~yShell();
+
+        
         //Every param's length must be < 100
+        /**
+         *  @fn      int RunShellCommand(std::vector<std::string> & cmd)
+         *  @brief   run user's cmd 
+         *  @param   cmd we want to run.
+         *  
+         *  @return The status of run-cmd
+         *  @retval 0 run cmd ok.
+         *  @retval -1 run cmd error.
+         * 
+         *  @warning This op is deprecated.
+         */
         __YLIB_DEPRECATED_ATTRIBUTE__ int RunShellCommand(std::vector<std::string> & cmd);
+
+        
         /*
             Default mode:
             cmd_'s size must be less than 20
@@ -50,19 +78,77 @@ namespace yLib{
             Extended mode:
             You can call SetUserModeConfig() reconfig them.
         */
+
+        /**
+         *  @fn      int8_t RunShellCommandEx(std::vector<std::string> & cmd_, std::vector<std::string> &cmd_env_, std::vector<std::string> & cmd_result_)
+         *  @brief   run user's cmd with env and get the result of rum-cmd.
+         * 
+         *  @param   cmd_ we want to run, cmd_'s size must be less than 20.
+         *  @param   cmd_env_ the env of cmd_, cmd_env_'s size must be less than 20.
+         *  @param   cmd_result_ the result of cmd_, _result_line_buf_size's size must be less than 1024.(_result_line_buf_size is max line-length of cmd_result).
+         * 
+         *  @return The status of run-cmd
+         *  @retval 0 run cmd ok.
+         *  @retval -1 run cmd error.
+         * 
+         *  @warning 
+         * 
+         */
         int8_t RunShellCommandEx(std::vector<std::string> & cmd_, std::vector<std::string> &cmd_env_, std::vector<std::string> & cmd_result_);
 
+        /**
+         *  @fn      void ReconfigyShellBuffer(uint64_t cmd_vec_size_, uint64_t cmd_env_vec_size_, uint64_t _result_line_buf_size_)
+         *  @brief   we can reconfig cmd_, cmd_env_, cmd_result_'s capcity.
+         *  @see RunShellCommandEx 's param.
+         *  @param   cmd_vec_size_ cmd-vec max size
+         *  @param   cmd_env_vec_size_ cmd-env-vec max size
+         *  @param   _result_line_buf_size_ result-line-buffer-vec max size
+         * 
+         * 
+         *  @return None.
+         * 
+         *  @warning 
+         */
         void ReconfigyShellBuffer(uint64_t cmd_vec_size_, uint64_t cmd_env_vec_size_, uint64_t _result_line_buf_size_);
 
         private:
-        
+        /** @var uint64_t cmd_vec_size
+            @brief The cmd_ max size.
+            @see RunShellCommand / RunShellCommandEx 's param
+        */
         uint64_t cmd_vec_size = 20;
+
+        /** @var uint64_t cmd_env_vec_size
+            @brief The cmd-env max size.
+            @see RunShellCommandEx 's param
+        */
         uint64_t cmd_env_vec_size = 20;
+
+        /** @var uint64_t _result_line_buf_size
+            @brief The result-buffer max size.
+
+            @see RunShellCommandEx 's param
+        */
         uint64_t _result_line_buf_size = 1024;
 
+        /** @var char * * parse_cmd_array
+            @brief The pointer of cmd-buffer parsed.
+            @see RunShellCommand / RunShellCommandEx 's param
+        */
         char * * parse_cmd_array = nullptr;
+
+        /** @var char * * parse_cmd_env_array
+            @brief The pointer of cmd-env-buffer parsed
+            @see RunShellCommandEx 's param
+        */
         char * * parse_cmd_env_array = nullptr;
+
+        /** @var char * result_read_buffer
+            @brief The pointer of result-buffer
+            @see RunShellCommandEx 's param
+        */
         char * result_read_buffer = nullptr;
+        
         protected:
     };
 
